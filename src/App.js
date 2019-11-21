f73c1d54802ca323164767f83ed305195a2707ae
pj-wise <51010291+pj-wise@users.noreply.github.com>
2019-11-20 23:07:04 -0500

    more tweaks, need to implement 2nd api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 //imports
@@ -7,14 +7,13 @@ import AddressGrid from "./components/AddressGrid";
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>Address.info</h1>
-        <AddressGrid />
-        <div className='thankyou'>
-          <h5>Created by: Pearl Wise</h5>
-          <h5>Thank you for your time & consideration</h5>
-        </div>
-      </header>
+      <h1>Address.info</h1>
+      <AddressGrid />
+
+      <div className='thankyou'>
+        <h5>Created by: Pearl Wise</h5>
+        <h5>Thank you for your time & consideration</h5>
+      </div>
     </div>
   );
 }
--- src/components/AddressGrid.js
+++ src/components/AddressGrid.js
@@ -7,14 +7,21 @@ import AddressCard from "./AddressCard";
 
 const AddressGrid = () => {
   const [addressData, setAddressData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [dataLimit, setDataLimit] = useState(9);
 
+  const changeLimit = () => {
+    setDataLimit(dataLimit + 3);
+  };
+
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         `https://wgrau8p1s0.execute-api.us-east-1.amazonaws.com/production/1/${dataLimit}/`
       )
       .then(res => {
+        setLoading(false);
         console.log(res.data.rows);
         setAddressData(res.data.rows);
       })
@@ -36,6 +43,16 @@ const AddressGrid = () => {
           />
         );
       })}
+      <button class='moreBtn' onClick={() => changeLimit()}>
+        View More
+      </button>
+      <h1
+        style={{
+          display: loading === true ? "block" : "none",
+          color: "#272343"
+        }}>
+        Loading Addresses
+      </h1>
     </div>
   );
 };
